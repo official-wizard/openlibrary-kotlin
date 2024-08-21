7c1a9dc5747966737dc9672bf604bcc78509b2a5
Wizard <lenkgit@gmail.com>
2024-08-21 08:19:24 -0400

    chore: add create, delete, & update list

--- src/main/kotlin/org/openlibrary/api/OpenLibraryInterface.kt
+++ src/main/kotlin/org/openlibrary/api/OpenLibraryInterface.kt
@@ -3,10 +3,10 @@ package org.openlibrary.api
 import co.infinum.retromock.meta.Mock
 import co.infinum.retromock.meta.MockResponse
 import com.haroldadmin.cnradapter.NetworkResponse
-import okhttp3.ResponseBody
 import org.openlibrary.api.data.pojo.account.Login
 import org.openlibrary.api.data.Partner
 import org.openlibrary.api.data.SearchChangesQuery
+import org.openlibrary.api.data.pojo.ErrorResponse
 import org.openlibrary.api.data.pojo.authors.SearchAuthors
 import org.openlibrary.api.data.pojo.authors.SearchAuthorsWorks
 import org.openlibrary.api.data.pojo.book.SearchBooks
@@ -18,10 +18,11 @@ import org.openlibrary.api.data.pojo.partner.SearchPartner
 import org.openlibrary.api.data.pojo.subjects.SearchSubjects
 import org.openlibrary.api.data.pojo.works.*
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -91,32 +92,32 @@ interface OpenLibraryInterface {
     @GET("/works/{workId}.json")
     suspend fun searchWorks(
         @Path("workId") workId: String,
-    ): NetworkResponse<SearchWorks.Response, SearchError.Response>
+    ): NetworkResponse<SearchWorks.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/works/SearchWorksEditions.json")
     @GET("/works/{olid}/editions.json")
     suspend fun searchWorksEditions(
         @Path("olid") olid: String,
         @Query("offset") offset: Long? = null
-    ): NetworkResponse<SearchWorksEditions.Response, SearchError.Response>
+    ): NetworkResponse<SearchWorksEditions.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/works/SearchWorksBookshelves.json")
     @GET("/works/{olid}/bookshelves.json")
     suspend fun searchWorksBookshelves(
         @Path("olid") olid: String,
-    ): NetworkResponse<SearchWorksBookshelves.Response, SearchError.Response>
+    ): NetworkResponse<SearchWorksBookshelves.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/works/SearchWorksRatings.json")
     @GET("/works/{olid}/ratings.json")
     suspend fun searchWorksRatings(
         @Path("olid") olid: String,
-    ): NetworkResponse<SearchWorksRatings.Response, SearchError.Response>
+    ): NetworkResponse<SearchWorksRatings.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/works/SearchIsbn.json")
     @GET("/isbn/{isbn}.json")
     suspend fun searchWorksIsbn(
         @Path("isbn") isbn: String
-    ): NetworkResponse<SearchIsbn.Response, SearchError.Response>
+    ): NetworkResponse<SearchIsbn.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/authors/SearchAuthors.json")
     @GET("/search/authors.json")
@@ -130,7 +131,7 @@ interface OpenLibraryInterface {
         @Path("olid") olid: String,
         @Query("limit") limit: Int = 50,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<SearchAuthorsWorks.Response, SearchError.Response>
+    ): NetworkResponse<SearchAuthorsWorks.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/subjects/SearchSubjects.json")
     @GET("/subjects/{subject}.json")
@@ -153,7 +154,7 @@ interface OpenLibraryInterface {
     @GET("/api/volumes/brief/json/{request-list}")
     suspend fun searchPartners(
         @Path("request-list") requestList: String
-    ): NetworkResponse<Map<String, SearchPartner.Response>, SearchError.Response>
+    ): NetworkResponse<Map<String, SearchPartner.Response>, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/changes/SearchRecentChanges.json")
     @GET("/recentchanges.json")
@@ -161,7 +162,7 @@ interface OpenLibraryInterface {
         @Query("bot") bot: Boolean? = null,
         @Query("limit") limit: Int = 50,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<List<SearchChanges.Response>, SearchError.Response>
+    ): NetworkResponse<List<SearchChanges.Response>, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/changes/SearchChanges.json")
     @GET("/recentchanges/{query}")
@@ -170,7 +171,7 @@ interface OpenLibraryInterface {
         @Query("bot") bot: Boolean? = null,
         @Query("limit") limit: Int = 50,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<List<SearchChanges.Response>, SearchError.Response>
+    ): NetworkResponse<List<SearchChanges.Response>, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchUsersList.json")
     @GET("/people/{username}/lists.json")
@@ -178,21 +179,21 @@ interface OpenLibraryInterface {
         @Path("username") username: String,
         @Query("limit") limit: Int = 50,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<SearchUsersList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchUsersOlidList.json")
     @GET("/people/{username}/lists/{olid}.json")
     suspend fun searchUsersList(
         @Path("username") username: String,
         @Path("olid") olid: String
-    ): NetworkResponse<SearchUserOlidList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUserOlidList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchUsersSeedList.json")
     @GET("/people/{username}/lists/{olid}/seeds.json")
     suspend fun searchUsersSeedList(
         @Path("username") username: String,
         @Path("olid") olid: String
-    ): NetworkResponse<SearchUsersSeedList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersSeedList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchUsersEditionList.json")
     @GET("/people/{username}/lists/{olid}/editions.json")
@@ -201,7 +202,7 @@ interface OpenLibraryInterface {
         @Path("olid") olid: String,
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<SearchUsersEditionList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersEditionList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchUsersSubjectsList.json")
     @GET("/people/{username}/lists/{olid}/subjects.json")
@@ -210,31 +211,31 @@ interface OpenLibraryInterface {
         @Path("olid") olid: String,
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<SearchUsersSubjectList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersSubjectList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchOlidBooksList.json")
     @GET("/books/{olid}/lists.json")
     suspend fun searchOlidBooksList(
         @Path("olid") olid: String
-    ): NetworkResponse<SearchUsersList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchOlidWorksList.json")
     @GET("/works/{olid}/lists.json")
     suspend fun searchOlidWorksList(
         @Path("olid") olid: String
-    ): NetworkResponse<SearchUsersList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchOlidAuthorsList.json")
     @GET("/authors/{olid}/lists.json")
     suspend fun searchOlidAuthorsList(
         @Path("olid") olid: String
-    ): NetworkResponse<SearchUsersList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchSubjectPlaceList.json")
     @GET("/subjects/place:{location}/lists.json")
     suspend fun searchSubjectsPlaceList(
         @Path("location") location: String
-    ): NetworkResponse<SearchUsersList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersList.Response, ErrorResponse.Response>
 
     @Mock @MockResponse(body = "/v1/list/SearchQueryList.json")
     @GET("/search/lists.json")
@@ -242,19 +243,36 @@ interface OpenLibraryInterface {
         @Query("q") query: String,
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 0
-    ): NetworkResponse<SearchUsersList.Response, SearchError.Response>
+    ): NetworkResponse<SearchUsersList.Response, ErrorResponse.Response>
 
     @POST("/account/login")
     @Headers("Content-Type: application/x-www-form-urlencoded")
     suspend fun authenticate(
         @Query("username") username: String,
         @Query("password") password: String,
-    ): NetworkResponse<Login.Response, Unit>
+    ): NetworkResponse<Login.Response, String>
 
     @POST("/people/{username}/lists")
     @Headers("Content-Type: application/json")
     suspend fun createList(
         @Path("username") username: String,
         @Body createList: CreateList.Request
-    ): NetworkResponse<CreateList.Response, Unit>
+    ): NetworkResponse<CreateList.Response, String>
+
+    @Deprecated("Doesn't support POST requests, although docs says it should!")
+    @POST("/people/{username}/list/{olid}/delete.json")
+    @Headers("Content-Type: application/json")
+    suspend fun deleteList(
+        @Path("username") username: String,
+        @Path("olid") olid: String
+    ): NetworkResponse<String, String>
+
+    @Deprecated("This is not yet implemented!")
+    @PUT("/people/{username}/list/{olid}")
+    @Headers("Content-Type: application/json")
+    suspend fun updateList(
+        @Path("username") username: String,
+        @Path("olid") olid: String,
+        @Body createList: CreateList.Request
+    ): NetworkResponse<CreateList.Response, String>
 }
